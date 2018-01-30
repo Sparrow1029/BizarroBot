51183b11c778a035c26f7328340b94e972569fbe
Sparrow1029 <arayart88@gmail.com>
2018-01-29 20:47:24 -0500

    Wrote TODO messages and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # BizarroBot
-Trump Twitter reverser
+
+####A Twitter bot that reverses the language of @realDonaldTrump's status updates
+####using Twython
+
+Oauth keys/tokens are stored as a dictionary in a `config.py` file in the app directory.
+
+Working on automating processes and using **MUCH** better regex to keep the tweets closer to their
+original format.
+
+Also need to parse the json data so the bot includes hashtags and media.
--- twythonApp/TwyBot.py
+++ twythonApp/TwyBot.py
@@ -7,37 +7,49 @@ import re
 twitter = Twython(keys['CONSUMER_KEY'], keys['CONSUMER_SECRET'],
                  keys['ACCESS_TOKEN'], keys['ACCESS_SECRET'])
 
-Trump = twitter.get_user_timeline(screen_name='realDonaldTrump',
+trump = twitter.get_user_timeline(screen_name='realDonaldtrump',
                                   count=5,tweet_mode='extended')
+
+bizarro = twitter.get_home_timeline()
+
+
 twitter.verify_credentials()
-for t in Trump:
+for t in trump:
     print(t['full_text'])
 sleep(2)
 antonyms = trumpsaurus().antonyms
 
 
 def swap_words(tweet, wordDict):
+    """Here's the bizarro switcher"""
     orig_text = tweet['full_text'].lower()
-    links = re.compile(r'[http|https]://.*')
+
+    # Removing links for now till I parse json object to relink hyperlinks
+    links = re.compile(r'http|https://.*')
     orig_text = links.sub('', orig_text)
+
+    # Found this on SO for swapping words out. It creates a huge list of
+    # OR `|` statements based on the dict keys for `re.sub`
     pattern = re.compile(r'\b(' + '|'.join(wordDict.keys()) + r')\b')
     result = pattern.sub(lambda x: wordDict[x.group()], orig_text)
     return str(result)
 
 
-for tweet in Trump:
+for tweet in trump:
         reversed_tweet = swap_words(tweet, antonyms)
         print(reversed_tweet)
+        # THIS doesn't work
+        # if reversed_tweet not in [s['text'] for s in bizarro]:
         post = twitter.update_status(status=reversed_tweet)
-        print(post)
+        #     print(post)
+        # else:
+        #     print('Tweet already exists.')
         sleep(4)
-  # words = list(map(lambda x: x.lower(), orig_text.split()))
-    # to_replace = []
 
-    # for word in words:
-    #     mo = re.match(r'\b\w*\b', word)
-    #     if mo:
-    #         print(mo.group(0))
-    #         if mo.group(0) in antonyms.keys():
-    #             to_replace.append(mo.group(0))
-    # print(to_replace)
+# TODO: Add handling for already existing statuses.
+# TODO: More specific about:
+#           - Checking rate-limit and staying within it
+#           - Including #hashtags and @handles and media links
+# TODO: Better regex and maintaining case of words
+# TODO: EVENTUALLY run a cron job on the server to check for new Trump
+#           tweets and run the script
